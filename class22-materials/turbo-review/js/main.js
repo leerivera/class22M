e7c948b8747e1864c268e3d35fec74606b022f66
leerivera <leerivera80@gmail.com>
2022-03-24 21:23:40 -0400

    second5

--- class22-materials/turbo-review/js/main.js
+++ class22-materials/turbo-review/js/main.js
@@ -11,16 +11,31 @@ console.log( string.search('apple') )
 
 // *Functions*
 // Create a function that returns rock, paper, or scissors as randomly as possible
+function paperRock(){
+    let random = Math.random()
 
-// *Conditionals*
-//Create a function that takes in a choice (r++k, paper, or scissors) and determines if they won a game of rock paper scissors against a bot using the above function
-function rockPaper(choice){
-     choice = prompt('rock paper scissors')
-
-    if(choice === "scissors"){
-        console.log("yo")
+    if(random < .33){
+        return 'rock'
+    } else if( random < .66){
+        return 'paper'
+    } else{
+        return 'scissors'
     }
 }
-rockPaper()
+// *Conditionals*
+//Create a function that takes in a choice (r++k, paper, or scissors) and determines if they won a game of rock paper scissors against a bot using the above function
+function checkWin(pChoice){
+  
+
+
+    let botChoice = paperRock()
+    if ( (pChoice === 'rock' && botChoice === 'scissors') || (pChoice === "paper" && botChoice ==='rock') || (pChoice ==='scissors' && botChoice === 'paper')){
+                   console.log("win!")
+    } else if ( pChoice === botChoice ){
+                   console.log('lose')
+    }
+}
+checkWin('rock')
+
 //*Loops*
 //Create a function that takes an array of choices. Play the game x times where x is the number of choices in the array. Print the results of each game to the console.
